226b965d674e68a86ad1b87b975969c8e42e4be1
Stackbit Code Editor <support@stackbit.com>
2021-03-10 21:37:58 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,6 +13,15 @@ export default class Body extends React.Component {
         return (
             <React.Fragment>
                 <Helmet>
+                    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FWWGZ3NJN1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FWWGZ3NJN1');
+</script>
                     <title>{_.get(this.props, 'pageContext.frontmatter.seo.title', null) ? (_.get(this.props, 'pageContext.frontmatter.seo.title', null)) : _.get(this.props, 'pageContext.frontmatter.title', null) + ' | ' + _.get(this.props, 'pageContext.site.siteMetadata.title', null)}</title>
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
