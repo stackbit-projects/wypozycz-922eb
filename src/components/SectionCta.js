d7a0e3272008da0da693660c874b23e69f31a0b7
Stackbit Code Editor <support@stackbit.com>
2021-03-10 22:38:40 +0000

    SectionCta.js: changed

--- src/components/SectionCta.js
+++ src/components/SectionCta.js
@@ -23,6 +23,7 @@ export default class SectionCta extends React.Component {
                   </div>
                   {_.get(section, 'actions', null) && (
                   <div className="cell block-buttons">
+                      <
                     <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
                   </div>
                   )}
