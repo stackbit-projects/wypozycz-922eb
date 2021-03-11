f6290d6640f39621b1fd9b1e07ed876a190b6e2b
Stackbit Code Editor <support@stackbit.com>
2021-03-11 21:15:08 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default class SignupForm extends React.Component {
     constructor(props) {
         super(props);
+        
         this.state = {
             email: ""
         };
