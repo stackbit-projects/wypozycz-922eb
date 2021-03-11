660a0516f59746b03a7faf175897cc17c68426c2
Stackbit Code Editor <support@stackbit.com>
2021-03-11 20:55:47 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -4,36 +4,51 @@ import _ from 'lodash';
 import FormField from './FormField';
 
 export default class SignupForm extends React.Component {
-    render() {
-        let state = {
-            email: ""
-        };
+this.state = {
+    name: "",
+    email: "",
+    feedback: "",
+};
+  }
+handleInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({ [name]: value });
+}
 
-        let registerEmail = (e) => {
-            e.preventDefault();
-            console.log(e.target.value);
-        };
 
-        let handleInputChange = (event) => {
-            event.preventDefault();
-            const target = event.target;
-            const name = target.name;
-            const value = target.value;
-            this.setState({ [name]: value });
-            console.log(state);
-        };
+render() {
+    let state = {
+        email: ""
+    };
 
-        return (
-            <section className="cell widget widget-form">
-                <form name="signup" id="signup" method="POST" action="/" onSubmit={registerEmail.bind(this)}>
-                    <div className="form-row">
-                        <input name="email" type="email" placeholder="Adres email" onChange={handleInputChange.bind(this)} required />
-                    </div>
-                    <div className="form-row">
-                        <button type="submit" className="button">Zapisz się</button>
-                    </div>
-                </form>
-            </section>
-        );
-    }
+    let registerEmail = (e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+    };
+
+    let handleInputChange = (event) => {
+        event.preventDefault();
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        this.setState({ [name]: value });
+        console.log(state);
+    };
+
+    return (
+        <section className="cell widget widget-form">
+            <form name="signup" id="signup" method="POST" action="/" onSubmit={registerEmail.bind(this)}>
+                <div className="form-row">
+                    <input name="email" type="email" placeholder="Adres email" onChange={handleInputChange.bind(this)} required />
+                </div>
+                <div className="form-row">
+                    <button type="submit" className="button">Zapisz się</button>
+                </div>
+            </form>
+        </section>
+    );
+}
 }
