d1453a05d7774b51f06aa747aed7eed286fbd1b0
Stackbit Code Editor <support@stackbit.com>
2021-03-11 22:02:29 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -15,7 +15,8 @@ export default class SignupForm extends React.Component {
         const requestOptions = {
             method: 'POST',
             headers: { 
-                'Content-Type': 'application/json' },
+                'Content-Type': 'application/json',
+             },
             body: JSON.stringify({ title: 'React POST Request Example' })
         };
         const response = await fetch('https://jsonplaceholder.typicode.com/posts', requestOptions);
