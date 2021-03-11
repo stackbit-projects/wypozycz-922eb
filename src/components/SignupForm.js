9dc8d142b78bcd35701585f4b5b30d89f3dd4bd2
Stackbit Code Editor <support@stackbit.com>
2021-03-11 20:58:36 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -30,7 +30,7 @@ export default class SignupForm extends React.Component {
             <section className="cell widget widget-form">
                 <form name="signup" id="signup" method="POST" action="/" onSubmit={this.registerEmail.bind(this)}>
                     <div className="form-row">
-                        <input name="email" type="email" placeholder="Adres email" onChange={handleInputChange.bind(this)} required />
+                        <input name="email" type="email" placeholder="Adres email" onChange={this.handleInputChange.bind(this)} required />
                     </div>
                     <div className="form-row">
                         <button type="submit" className="button">Zapisz się</button>
