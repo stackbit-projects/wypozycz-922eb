984f388fbcc95303eafafa879026cc74351e1caf
Stackbit Code Editor <support@stackbit.com>
2021-03-11 20:39:49 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -14,6 +14,7 @@ export default class SignupForm extends React.Component {
         };
 
         let registerEmail = (e) => {
+            e.preventDefault();
             alert('works');
         };
 
