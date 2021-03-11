ff56bea0dd7d5600e2acb2d62d3bf5da16d7c3b7
Stackbit Code Editor <support@stackbit.com>
2021-03-11 21:07:26 +0000

    gatsby-ssr.js: changed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,27 +7,27 @@
 const React = require("react");
 const withPrefix = require("./src/utils/withPrefix").default;
 
-exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        <React.Fragment>         
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XGP4576NPQ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
+        <React.Fragment>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-XGP4576NPQ"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-XGP4576NPQ');
+                gtag('js', new Date());
+              
+                gtag('config', 'G-XGP4576NPQ');
 </script>
         </React.Fragment>
     ]);
 
     setPostBodyComponents([
         <React.Fragment>
-            <script src={withPrefix('js/plugins.js')}/>
-            <script src={withPrefix('js/init.js')}/>
-            <script src={withPrefix('js/main.js')}/>
-            
+            <script src={withPrefix('js/plugins.js')} />
+            <script src={withPrefix('js/init.js')} />
+            <script src={withPrefix('js/main.js')} />
+
         </React.Fragment>
     ]);
 
