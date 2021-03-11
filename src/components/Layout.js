9cb830d07552ecf5f8f51167966f6e1075380fa9
Stackbit Code Editor <support@stackbit.com>
2021-03-11 21:20:59 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,11 +18,12 @@ export default class Body extends React.Component {
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
                     <script async src="https://www.googletagmanager.com/gtag/js?id=G-XGP4576NPQ"></script>
-                    <script>{`window.dataLayer = window.dataLayer || [];
+                    <script>
+                        {`window.dataLayer = window.dataLayer || [];
                 function gtag(){window.dataLayer.push(arguments);}
                 gtag('js', new Date());              
                 gtag('config', 'G-XGP4576NPQ');`}
-                </script>
+                    </script>
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.seo.description', null) || ''} />
                     {_.get(this.props, 'pageContext.frontmatter.seo.robots', null) && (
                         <meta name="robots" content={_.join(_.get(this.props, 'pageContext.frontmatter.seo.robots', null), ',')} />
