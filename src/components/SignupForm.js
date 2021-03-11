26e27f11ef0ec49429aae471c79c0f35ba01d5fd
Stackbit Code Editor <support@stackbit.com>
2021-03-11 20:27:52 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -10,10 +10,7 @@ export default class FooterForm extends React.Component {
         return (
             <section className="cell widget widget-form">              
               <h2 className="widget-title">Zapisz się</h2>              
-              <form name="signup" id="signup" method="POST" action="/">               
-                <input aria-labelledby={_.get(section, 'form_id', null) + '-honeypot-label'} type="hidden" name="form-name"
-                  value={_.get(section, 'form_id', null)} />
-                {_.map(_.get(section, 'form_fields', null), (field, field_idx) => (
+              <form name="signup" id="signup" method="POST" action="/">                               
                 <div key={field_idx} className="form-row">
                   <FormField {...this.props} field={field} />
                 </div>
