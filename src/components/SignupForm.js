dfddb9ef5a42e81cd3c6e5791ef48b9e2463c547
Stackbit Code Editor <support@stackbit.com>
2021-03-11 20:35:26 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -12,7 +12,7 @@ export default class FooterForm extends React.Component {
             is_required: true,
         };
 
-        registerEmail = (e) => {};
+        function registerEmail = (e) => {};
 
         return (
             <section className="cell widget widget-form">
