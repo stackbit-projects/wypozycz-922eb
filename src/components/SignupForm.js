75661f090f48ac0904c514ad206efa339261e2c2
Stackbit Code Editor <support@stackbit.com>
2021-03-11 20:36:53 +0000

    SignupForm.js: changed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -17,7 +17,7 @@ export default class SignupForm extends React.Component {
 
         return (
             <section className="cell widget widget-form">
-                <form name="signup" id="signup" method="POST" action="/" onSubmit={this.registerEmail.bind(this)}>
+                <form name="signup" id="signup" method="POST" action="/" onSubmit={self.registerEmail.bind(this)}>
                     <div className="form-row">
                         <FormField {...this.props} field={field} />
                     </div>
